c3b5bdd292187b4ae01b69dfbdc023ed7a8869d7
PaarthAggarwal19 <77836822+PaarthAggarwal19@users.noreply.github.com>
2021-02-02 17:44:13 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,23 @@
+var a,b
+function setup() {
+  createCanvas(800,400);
+  a= createSprite (500,300,50,50)
+  b=createSprite(400, 200, 50, 50);
+  a.shapeColor="green"
+  b.shapeColor ="green"
+}
+
+function draw() {
+  background("black");  
+  a.x = mouseX
+  a.y = mouseY 
+  if(a.x-b.x<a.width/2+b.width/2 && b.x-a.x<a.width/2+b.width/2 && a.y-b.y<a.height/2+b.height/2 && b.y-a.y<a.height/2+b.height/2){
+    a.shapeColor = "blue"
+    b.shapeColor = "blue" 
+  }
+  else{
+  a.shapeColor = "green"  
+  b.shapeColor = "green"
+  }
+  drawSprites();
+}
